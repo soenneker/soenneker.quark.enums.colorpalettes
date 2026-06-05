579689bd41e0a11079a6ca8802799b173da79c01
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:14 -0500

    xml commenting

--- src/Soenneker.Quark.Enums.ColorPalettes/ColorPaletteEnum.cs
+++ src/Soenneker.Quark.Enums.ColorPalettes/ColorPaletteEnum.cs
@@ -8,26 +8,92 @@ namespace Soenneker.Quark;
 [EnumValue<string>]
 public sealed partial class ColorPaletteEnum
 {
+    /// <summary>
+    /// The slate.
+    /// </summary>
     public static readonly ColorPaletteEnum Slate = new("slate");
+    /// <summary>
+    /// The gray.
+    /// </summary>
     public static readonly ColorPaletteEnum Gray = new("gray");
+    /// <summary>
+    /// The zinc.
+    /// </summary>
     public static readonly ColorPaletteEnum Zinc = new("zinc");
+    /// <summary>
+    /// The neutral.
+    /// </summary>
     public static readonly ColorPaletteEnum Neutral = new("neutral");
+    /// <summary>
+    /// The stone.
+    /// </summary>
     public static readonly ColorPaletteEnum Stone = new("stone");
+    /// <summary>
+    /// The red.
+    /// </summary>
     public static readonly ColorPaletteEnum Red = new("red");
+    /// <summary>
+    /// The orange.
+    /// </summary>
     public static readonly ColorPaletteEnum Orange = new("orange");
+    /// <summary>
+    /// The amber.
+    /// </summary>
     public static readonly ColorPaletteEnum Amber = new("amber");
+    /// <summary>
+    /// The yellow.
+    /// </summary>
     public static readonly ColorPaletteEnum Yellow = new("yellow");
+    /// <summary>
+    /// The lime.
+    /// </summary>
     public static readonly ColorPaletteEnum Lime = new("lime");
+    /// <summary>
+    /// The green.
+    /// </summary>
     public static readonly ColorPaletteEnum Green = new("green");
+    /// <summary>
+    /// The emerald.
+    /// </summary>
     public static readonly ColorPaletteEnum Emerald = new("emerald");
+    /// <summary>
+    /// The teal.
+    /// </summary>
     public static readonly ColorPaletteEnum Teal = new("teal");
+    /// <summary>
+    /// The cyan.
+    /// </summary>
     public static readonly ColorPaletteEnum Cyan = new("cyan");
+    /// <summary>
+    /// The sky.
+    /// </summary>
     public static readonly ColorPaletteEnum Sky = new("sky");
+    /// <summary>
+    /// The blue.
+    /// </summary>
     public static readonly ColorPaletteEnum Blue = new("blue");
+    /// <summary>
+    /// The indigo.
+    /// </summary>
     public static readonly ColorPaletteEnum Indigo = new("indigo");
+    /// <summary>
+    /// The violet.
+    /// </summary>
     public static readonly ColorPaletteEnum Violet = new("violet");
+    /// <summary>
+    /// The purple.
+    /// </summary>
     public static readonly ColorPaletteEnum Purple = new("purple");
+    /// <summary>
+    /// The fuchsia.
+    /// </summary>
     public static readonly ColorPaletteEnum Fuchsia = new("fuchsia");
+    /// <summary>
+    /// The pink.
+    /// </summary>
     public static readonly ColorPaletteEnum Pink = new("pink");
+    /// <summary>
+    /// The rose.
+    /// </summary>
     public static readonly ColorPaletteEnum Rose = new("rose");
 }
